0d2ca3dcdd88675d35e888faebfe6baf68fbfaaf
mizanurhasan0 <sajal596@diu.edu.bd>
2025-06-22 15:54:00 +0600

    SSL key function add

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,10 +2,12 @@ import { db } from "./db";
 import { env } from "./env";
 import { jwt } from "./jwt";
 import { mail } from "./mail";
+import { getHttpsKeys } from "./ssl";
 
 export const config = {
     env,
     db,
     mail,
-    jwt
+    jwt,
+    getHttpsKeys
 }
